cb71c46f66a9f0d5dcef8bd2be7dcdda4abf296d
Gustavo Higuera <ghiguera@ucol.mx>
2020-12-09 13:08:54 -0600

    implemented sumatoriaSerieUno()

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
-export default class App {}
+export default class App {
+
+sumatoriaSerieUno(numero){
+    
+    let suma=0;
+    for(let i=1; i<=numero; i++){
+        suma= suma + (1/i);
+    }
+    return suma;
+}
+
+
+
+
+
+
+
+
+
+
+}
 
 let app = new App();
+
+console.log(app.sumatoriaSerieUno(6));
